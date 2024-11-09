82398e7fc8220dd19cbb0008596612c445684af5
Llamato <llamato@noreply.github.com>
2024-11-09 16:32:11 +0100

    added ability to make code disk to build system

---
+++ buildscripts/packasmdisk.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+def syscmd(cmd):
+    print(cmd)
+    os.system(cmd)
+
+filenames = os.listdir(".")
+if len(filenames) == 0:
+    print("No asm files found. Aborting...", file=sys.stderr)
+    exit(1)
+
+syscmd("c1541 -format asmdisk,01 d64 build/asmdisk.d64")
+for filename in filenames:
+    if filename.endswith(".asm"):
+        #syscmd("python3 buildscripts/C64AsciiPetsciiConverter/src/python/asciitopetscii.py " + filename + " buildscripts/" + filename)
+        syscmd(f'petcat -text -w2 -o build/{filename} {filename}')
+        syscmd(f'c1541 -attach build/asmdisk.d64 -write build/{filename} {filename},s')
+        if len(filename) > 16:
+            print("Warning: file name too long", file=sys.stderr)
