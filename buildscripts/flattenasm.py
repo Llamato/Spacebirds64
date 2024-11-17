0137d7135f97c3674ee9f1408f4dcd28277e1baa
Tina <llamato@users.noreply.github.com>
2024-11-17 16:00:52 +0100

    implemented .include flattening pack option

---
+++ buildscripts/flattenasm.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+def flattenIncludes(file_path):
+    output_string = ""
+    input_file = None
+    try:
+        input_file = open(file_path)
+        for line in input_file.readlines():
+            if line.startswith(".include "):
+                include_path = line.split()[1].replace('"', '')
+                include_output = flattenIncludes(include_path)
+                if include_output == "":
+                    output_string += line
+                else:
+                    output_string += include_output
+            else:
+                output_string += line
+        input_file.close()
+        return output_string
+    except FileNotFoundError:
+        print("Error:", file_path, "not found!", file=sys.stderr)
+        return ""
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please specifiy input file and optionally output file as first and second parameter, respectively.", file=sys.stderr)
+        exit(0)
+    output = flattenIncludes(sys.argv[1])
+    if len(sys.argv) == 2:
+        print(output)
+        exit(0)
+    if len(sys.argv) > 2:
+        output_file = open(sys.argv[2], "w", newline='\n')
+        print(output, file=output_file)
+    
--- buildscripts/packasmdisk.py
+++ buildscripts/packasmdisk.py
@@ -1,20 +1,46 @@
 import os
 import sys
+import pathlib
+from flattenasm import flattenIncludes
 
 def syscmd(cmd):
     print(cmd)
     os.system(cmd)
 
-filenames = os.listdir(".")
-if len(filenames) == 0:
-    print("No asm files found. Aborting...", file=sys.stderr)
-    exit(1)
-syscmd("c1541 -format asmdisk,01 d64 build/asmdisk.d64")
-preprocess_path = "build/preprocess"
+def pack_code_disk(code_path, pack_path, img_name):
+    filenames = os.listdir(code_path)
+    if len(filenames) == 0:
+        print("No asm files found. Aborting...", file=sys.stderr)
+        exit(1)
+    syscmd("c1541 -format asmdisk,01 d64 " + os.path.join(pack_path, img_name))
+    for filename in filenames:
+        if filename.endswith(".asm"):
+            syscmd(f'petcat -text -w2 -o {os.path.join(pack_path, filename)} {os.path.join(code_path, filename)}')
+            syscmd(f'c1541 -attach {os.path.join(pack_path, img_name)} -write {os.path.join(pack_path, filename)} {filename},s')
+            if len(filename) > 16:
+                print("Warning: file name too long", file=sys.stderr)
 
-for filename in filenames:
-    if filename.endswith(".asm"):
-        syscmd(f'petcat -text -w2 -o build/{filename} {filename}')
-        syscmd(f'c1541 -attach build/asmdisk.d64 -write build/{filename} {filename},s')
-        if len(filename) > 16:
-            print("Warning: file name too long", file=sys.stderr)
+def copy_code(src, dest, exclude=[]):
+    if not os.path.exists(dest):
+        pathlib.Path.mkdir(dest)
+    filenames = os.listdir(".")
+    for filename in filenames:
+        if filename.endswith(".asm") and filename not in exclude:
+            src_file = open(os.path.join(src, filename))
+            dest_file = open(os.path.join(dest, filename), "w", newline='\n')
+            dest_file.writelines(src_file.readlines())
+            src_file.close()
+            dest_file.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1].endswith(".asm"):
+        input_filepath = sys.argv[1]
+        preprocessor_path = os.path.join("build", "pre")
+        copy_code(".", preprocessor_path)
+        output_filepath = os.path.join(preprocessor_path, f'flat{input_filepath}')
+        output_file = open(output_filepath, "w", newline='\n')
+        output_file.write(flattenIncludes(input_filepath))
+        output_file.close()
+        pack_code_disk(preprocessor_path, "build", "asmdisk.d64")
+    else:
+        pack_code_disk(".", "build", "asmdisk.d64")
