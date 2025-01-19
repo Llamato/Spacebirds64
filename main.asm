a4a9e0c8425ac3caef3aa60954d29c782eecdb74
Max Brockmann <max@tbrockmann.de>
2025-01-19 10:46:57 +0100

    move fuel bar logic into own file

--- main.asm
+++ main.asm
@@ -158,109 +158,10 @@ sss
 .endif
 
 
-;---------- init fuel bar --------------
-initfuel
-.block
-ldx #0
-loop
-    ; draw full characters
-    lda #92
-    sta $0400 + 911, x
-
-    ; set the color
-    lda #$02
-    sta $D800 + 911, x
-
-    inx
-    cpx #8 ;fuel bar is 8 characters wide
-    bne loop
-
-    ; draw border on the right side
-    lda #84
-    sta $0400 + 919
-
-    ; set the color
-    lda #$01
-    sta $D800 + 919
-.bend
-
-jsr waitforinput
-
-;---------- reduce fuel ----------------
-reducefuel
-.block
-    lda fuel
-    beq outoffuel
-    dec fuel
-
-    ;divide by 8 to know how many
-    ;full characters we have to draw
-    lda fuel
-    lsr
-    lsr
-    lsr
-
-    ; save the result in x and y
-    tax 
-    tay 
-
-drawfullfuel
-    cpx #0
-    beq finishdrawing
-    dex 
-
-    ; draw full characters
-    lda #92
-    sta $0400 + 911, x
-
-    ; set the color
-    lda #$05
-    sta $D800 + 911, x
-
-    jmp drawfullfuel
-finishdrawing
 
 
 
-tya ; transfer the division result to x
-tax
-drawemptyfuel
-    cpx #8
-    beq finishdrawempty
-    lda #23  ; draw space -> empty fuel
-    sta $0400 + 911, x
-
-    inx
-    jmp drawemptyfuel
-
-finishdrawempty
-
-; get remainder {x AND (y-1)}
-lda fuel
-and #$07
-beq end ;zero was already drawn
-
-; draw the last character
-adc #83 ; add offset to get the right character
-sta $0400 + 911, y
-
-; set the color
-lda #$07
-sta $D800 + 911, y
-
-
-
-end
-    ;rts
-
-outoffuel
-
-    ; game over
-    ;jmp gameover
-.bend
-
-
-
+jsr initfuel
 
 
 
@@ -304,6 +205,11 @@ jumppad
 lda 198
 bne sshss
 #poke 198, 0
+
+jsr waitforinput
+jsr reducefuel
+
+
 jmp gameloop
 
 ;sshss = show save high score screen
@@ -581,6 +487,7 @@ next
 .include "dataflowsubs.asm"
 .include "playsid.asm"
 .include "disksubs.asm"
+.include "fuelbar.asm"
 
 ;Data
 ;tyfps = thank you for playing string
@@ -611,5 +518,3 @@ yearstring
 
 scorestring
 .null "Score: "
-
-fuel .byte 2  ;set fuel to max (64)
